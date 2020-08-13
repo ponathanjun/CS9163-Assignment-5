2ee95df73dd4274e76d80d439ad4acaff695e2cb
Jonathan Pun <jonathan.pun88@gmail.com>
2020-08-13 13:00:04 -0400

    Removed unneccesary permissions

--- app/src/main/java/edu/nyu/appsec/assignment5/MainActivity.java
+++ app/src/main/java/edu/nyu/appsec/assignment5/MainActivity.java
@@ -101,8 +101,8 @@ public class MainActivity extends AppCompatActivity {
         settings.setAllowFileAccessFromFileURLs(true);
         settings.setJavaScriptEnabled(true);
         settings.setAllowUniversalAccessFromFileURLs(true);
-*/
-        /*
+
+
         LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         if (!(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
                 && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)) {
